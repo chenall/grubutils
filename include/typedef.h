eb1821f70a46bc795d5de754bcb424bfbeb0f0b6
chenall <chenall.cn@gmail.com>
2014-09-22 15:09:07 +0800

    add include grub4dos.h

---
+++ include/typedef.h
@@ -0,0 +1,11 @@
+#ifndef __TYPEDEF__
+#define __TYPEDEF__
+typedef unsigned long       DWORD;
+typedef int                 BOOL;
+typedef unsigned char       BYTE;
+typedef unsigned short      WORD;
+typedef unsigned long long UINT64;
+typedef unsigned long		UINT32;
+typedef unsigned int			UINT;
+typedef unsigned char*		PBYTE;
+#endif
