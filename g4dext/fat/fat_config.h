239b2b5cdd051945280f80726f0a33d05ba3206f
chenall <chenall.cn@gmail.com>
2015-02-15 10:26:17 +0800

    add fat source

---
+++ g4dext/fat/fat_config.h
@@ -0,0 +1,7 @@
+#define _TIME_Y 2015
+#define _TIME_m 2
+#define _TIME_d 15
+#define _TIME_H 10
+#define _TIME_M 25
+#define _TIME_S 34
+#define GET_FATTIME
---
+++ g4dext/fat/option/diskio.c
@@ -0,0 +1,114 @@
+/*-----------------------------------------------------------------------*/
+/* Low level disk I/O module skeleton for FatFs     (C)ChaN, 2007        */
+/*-----------------------------------------------------------------------*/
+/* This is a stub disk I/O module that acts as front end of the existing */
+/* disk I/O modules and attach it to FatFs module with common interface. */
+/*-----------------------------------------------------------------------*/
+
+#include "diskio.h"
+/*-----------------------------------------------------------------------*/
+/* Correspondence between physical drive number and physical drive.      */
+/*-----------------------------------------------------------------------*/
+
+#define ATA		0
+#define MMC		1
+#define USB		2
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Inidialize a Drive                                                    */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_initialize (
+	BYTE drv				/* Physical drive nmuber (0..) */
+)
+{
+	if (! real_open_partition(0))
+		return STA_NOINIT;
+	return 0;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Return Disk Status                                                    */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_status (
+	BYTE drv		/* Physical drive nmuber (0..) */
+)
+{
+	if (! real_open_partition(0))
+		return STA_NOINIT;
+	return 0;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s)                                                        */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_read (
+	BYTE drv,		/* Physical drive nmuber (0..) */
+	BYTE *buff,		/* Data buffer to store read data */
+	DWORD sector,	/* Sector address (LBA) */
+	BYTE count		/* Number of sectors to read (1..255) */
+)
+{
+	if (! devread ((unsigned long long)sector, 0L, (unsigned long long)count << _SS_BIT ,(unsigned long long)(unsigned int)buff, GRUB_READ))
+	{
+		return RES_PARERR;
+	}
+	return RES_OK;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s)                                                       */
+/*-----------------------------------------------------------------------*/
+/* The FatFs module will issue multiple sector transfer request
+/  (count > 1) to the disk I/O layer. The disk function should process
+/  the multiple sector transfer properly Do. not translate it into
+/  multiple single sector transfers to the media, or the data read/write
+/  performance may be drasticaly decreased. */
+
+#if _READONLY == 0
+DRESULT disk_write (
+	BYTE drv,			/* Physical drive nmuber (0..) */
+	const BYTE *buff,	/* Data to be written */
+	DWORD sector,		/* Sector address (LBA) */
+	BYTE count			/* Number of sectors to write (1..255) */
+)
+{
+	if (! devread ((unsigned long)sector, 0L,(unsigned long long)count << _SS_BIT ,(unsigned long long)(unsigned int)buff, GRUB_WRITE))
+	{
+		return RES_PARERR;
+	}
+	return RES_OK;
+}
+#endif /* _READONLY */
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_ioctl (
+	BYTE drv,		/* Physical drive nmuber (0..) */
+	BYTE ctrl,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+	return 0;
+	//return RES_PARERR;
+}
+
+DWORD get_fattime (void)
+{
+	return 0;
+};
+
